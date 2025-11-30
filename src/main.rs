5f21047e2ce52dc14d3291a21a38c57e38bc1f71
Sebastien Dunne <dunn0203@algonquinlive.com>
2025-11-30 11:47:38 -0500

    added workflows

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use product_service::{
-    configuration::Settings,
-    startup::run
-};
 use env_logger::Env;
+use product_service::{configuration::Settings, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    let settings = Settings::new()
-        .set_wasm_rules_engine(false);
+    println!("I updated the code");
+    let settings = Settings::new().set_wasm_rules_engine(false);
     env_logger::init_from_env(Env::default().default_filter_or(&settings.log_level));
     run(settings)?.await
 }
